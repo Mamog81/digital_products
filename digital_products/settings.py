6992340fdabf6a78bc771306ed4277cea99a8307
Mamog <mohammadamin.mog@gmail.com>
2025-09-16 13:39:46 +0330

    product model was added

--- digital_products/settings.py
+++ digital_products/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 from decouple import config, Csv
 
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG")
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS")
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [x.strip() for x in v.split(',')])
 
 
 # Application definition
@@ -119,6 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+
+#Media Files
+MEDIA_ROOT = os.path.join(BASE_DIR , 'media/')
+MEDIA_URL = 'media/'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- products/models.py
+++ products/models.py
@@ -3,13 +3,51 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Category(models.Model):
+    parrent = models.ForeignKey('self', verbose_name=_("parrent"), on_delete=models.CASCADE , blank=True , null=True)
     title = models.CharField(_("title"), max_length=50)
-    description = models.TextField(_("description") , blank=True )
-
-
+    description = models.TextField(_("description") , blank=True ) 
+    avatar = models.ImageField(_("avatar"), upload_to='categories')
+    is_enable = models.BooleanField(_("is enable") , default=True  )
+    created_time = models.DateTimeField(_("created at"), auto_now_add=True)
+    updated_time = models.DateTimeField(_("updated at"), auto_now=True)
+    
+    class Meta:
+        db_table = 'categories'
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        
+    
 class Product(models.Model):
-    pass
+    title = models.CharField(_("title"), max_length=50)
+    description = models.TextField(_("description") , blank=True ) 
+    avatar = models.ImageField(_("avatar"), upload_to='categories')
+    is_enable = models.BooleanField(_("is enable") , default=True  )
+    categories = models.ManyToManyField("Category", verbose_name=_("categories") , blank=True)
+    created_time = models.DateTimeField(_("created at"), auto_now_add=True)
+    updated_time = models.DateTimeField(_("updated at"), auto_now=True)
+    
+    class Meta:
+        db_table = 'products'
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+        
+    
+
+
 
 
 class File(models.Model):
-    pass
+    product = models.ForeignKey("Product", verbose_name=_("product"), on_delete=models.CASCADE)
+    title = models.CharField(_("title"), max_length=50)
+    models.FileField(_("file"), upload_to='files/%Y/%m/%d/')
+    is_enable = models.BooleanField(_("is enable") , default=True  )
+    created_time = models.DateTimeField(_("created at"), auto_now_add=True)
+    updated_time = models.DateTimeField(_("updated at"), auto_now=True)
+    
+        
+    class Meta:
+        db_table = 'files'
+        verbose_name = 'File'
+        verbose_name_plural = 'Files'
+        
+    
